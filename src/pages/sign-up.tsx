e38cba39971e40684f02a7dd718c98518818ee34
Joseph <anselljoseph19@gmail.com>
2024-03-03 18:50:13 +0000

    set up handle submit and navigate to quiz page

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -7,63 +7,69 @@ import FormWrapper from "@/components/FormWrapper";
 import Button from "@mui/material/Button";
 import { useCookies } from "react-cookie";
 
-import Link from "next/link";
-
-const LoginPage: React.FC = (props) => {
+const LoginPage: React.FC = () => {
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [error, setError] = useState<string>("");
   const [cookies, setCookie] = useCookies(["logged_in"]);
 
-  //   const { login } = useAuth();
-
   const router = useRouter();
-
-  //   async function handleSubmit() {
-  //     try {
-  //       setError("");
-  //       await login(email, password);
-  //       router.push("/start");
-  //     } catch {
-  //       setError("Incorrect Email address or password");
-  //     }
-  //   }
-
-  const handleSubmit = () => {
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
     console.log("handleSubmit");
-    setCookie("logged_in", "false");
+    console.log("firstName", firstName);
+    console.log("lastName", lastName);
+    console.log("email", email);
+
+    setCookie("logged_in", "true");
+    router.push("/quiz");
   };
 
   return (
     <Layout>
       <h1>Sign up</h1>
       <FormWrapper>
-        <FormControl fullWidth>
-          <TextField
-            fullWidth
-            id="first-name"
-            label="First name"
-            variant="outlined"
-            margin="normal"
-          />
-          <TextField
-            fullWidth
-            id="last-name"
-            label="Last name"
-            variant="outlined"
-            margin="normal"
-          />
-          <TextField
-            fullWidth
-            id="email"
-            label="Email"
-            variant="outlined"
-            margin="normal"
-          />
-          <Button onClick={handleSubmit} variant="contained">
-            Submit
-          </Button>
-        </FormControl>
+        <form onSubmit={handleSubmit}>
+          <FormControl fullWidth>
+            <TextField
+              fullWidth
+              id="first-name"
+              label="First name"
+              variant="outlined"
+              margin="normal"
+              required
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+            />
+            <TextField
+              fullWidth
+              id="last-name"
+              label="Last name"
+              variant="outlined"
+              margin="normal"
+              required
+              onChange={(e) => {
+                setLastName(e.target.value);
+              }}
+            />
+            <TextField
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              variant="outlined"
+              margin="normal"
+              type="email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <Button type="submit" variant="contained">
+              Submit
+            </Button>
+          </FormControl>
+        </form>
       </FormWrapper>
     </Layout>
   );
